c12514f89aa5eab844658f9ae8bdf940c6affa1a
Valderrama20 <jose23122009@gmai.com>
2023-04-10 23:10:49 -0300

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Login from './componetes/login/login'
 import Nav from './componetes/nav/Nav'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Home from './componetes/home/Home'
