5865e0c0827fff339af7f0be385e550d56e3111a
Valderrama20 <jose23122009@gmai.com>
2023-04-27 12:06:32 -0300

    arrelgando detalles

--- client/src/Pages/Prueba.jsx
+++ client/src/Pages/Prueba.jsx
@@ -1,10 +1,11 @@
+import Nav from "../componetes/nav/Nav"
 import Orders from "../componetes/orders/Orders"
 
 
 
 export default function Prueba () {
     return <div>
+        <Nav/>
          <Orders/>
-        <h1>jose</h1>
     </div>
 }
